717fc5ad956f80640d335ee72a31a086c3c764db
vitimario <vitimario2@gmail.com>
2017-02-13 14:07:51 +0100

    added capture_to_stream.py

---
+++ camera_testing/caputre_to_stream.py
@@ -0,0 +1,13 @@
+import time
+import picamera
+
+def capture_to_stream(stream):
+    # Create an in-memory stream
+    with picamera.PiCamera() as camera:
+        camera.start_preview()
+        # Camera warm-up time
+        time.sleep(2)
+        camera.capture(stream, 'jpeg')
+
+my_stream = io.BytesIO()
+print my_stream
