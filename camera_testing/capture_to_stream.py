de3b797a79641cb63c160f9a9e993c18b43787a7
vitimario <vitimario2@gmail.com>
2017-02-13 15:15:08 +0100

    added processor experimental file

--- camera_testing/capture_to_stream.py
+++ camera_testing/capture_to_stream.py
@@ -1,14 +1,13 @@
 import io
 import time
 import picamera
+import cv2
+import numpy as np
 
-def capture_to_stream(stream):
+def capture_to_stream(stream = io.BytesIO()):
     # Create an in-memory stream
     with picamera.PiCamera() as camera:
         camera.start_preview()
         # Camera warm-up time
         time.sleep(2)
         camera.capture(stream, 'jpeg')
-
-my_stream = io.BytesIO()
-print my_stream
