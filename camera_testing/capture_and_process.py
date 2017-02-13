ea80c94e4f697e4f2c3cc100fecd4edae43f9dde
vitimario <vitimario2@gmail.com>
2017-02-13 20:52:12 +0100

    update

--- camera_testing/capture_and_process.py
+++ camera_testing/capture_and_process.py
@@ -40,11 +40,12 @@ class ImageProcessor(threading.Thread):
                     # Read the image and do some processing on it
                     data = np.fromstring(self.stream.getvalue(), dtype=np.uint8)
                     self.image = cv2.imdecode(data, 1)
+                    edges = cv2.Canny(self.image,100,200)
+
                     with image_lock: ## is not thread safe!!!!!
-                        cv2.imshow('image',self.image)
-                        k = cv2.waitKey(1)
-                        if k == 27:         # wait for ESC key to exit
-                            cv2.destroyAllWindows()
+                        cv2.imshow('image',edges)
+                        key = cv2.waitKey(1) & 0xFF
+                        if k == 27 or key == ord("q"):
                             done = True
                     # Set done to True if you want the script to terminate
                     # at some point
