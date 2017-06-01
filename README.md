e3bd5e5846385ef2138ac220c0f5c8ee9491faa5
marioviti <vitimario2@gmail.com>
2017-06-01 18:41:24 +0000

    updated

--- README.md
+++
@@ -1,4 +0,0 @@
-# RaspberryVision
-Python code for raspberry vision service
-
-kernprof -v -l test.py tag_detection Test_robust_Tags_with_id_skew.png
--- camera_tools/image_utils.py
+++ camera_tools/image_utils.py
@@ -1,6 +1,13 @@
 import numpy as np
 import cv2
 
+def high_contrast(image):
+    mx,mn = np.max(image),np.min(image)
+    image = (image - mn)/float(mx-mn)
+    image = image**2
+    image = (image)*255
+    return np.array((image),dtype=np.uint8)
+				
 def convert_to_HSV(image):
     """
         convert image to HSV color space
@@ -32,6 +39,20 @@ def show_image(image,window='image',still=True,fps=25.):
     cv2.destroyAllWindows()
     #cv2.waitKey(int((1/fps)*1000)) # in ms
 
+def automatic_canny(gray_image):
+    """
+        Use Canny edge detction algorythm
+    """
+    y,x = gray_image.shape
+    win_frac = 3
+    l_image = gray_image[y/win_frac:y-(y/win_frac),(x/win_frac):x/(win_frac-1)-((x/win_frac))]
+    r_image = gray_image[y/win_frac:y-(y/win_frac),x/(win_frac-1)-(x/win_frac):x-(x/win_frac)]
+    v = (np.mean(l_image) + np.mean(r_image))/2.
+    lower = int(max(0, (1.0 - sigma) * v))
+    upper = int(min(255, (1.0 + sigma) * v))
+    edge_image = cv2.Canny(gray_image, lower, upper)
+    return edge_image
+
 def read_image(path):
     """
     load as UNIT8 image
@@ -45,7 +66,7 @@ def threshold(image,t):
     ret,thresh = cv2.threshold(image,t,255,0)
     return thresh
 
-def resize_image(image,fx=0.5,fy=0.5,interpolation=cv2.INTER_LINEAR):
+def resize_image(image,fx=0.5,fy=0.5,interpolation=cv2.INTER_NEAREST):
     return cv2.resize(image,None,fx=fx,fy=fy,interpolation=interpolation)
 
 def convert_grey(image):
